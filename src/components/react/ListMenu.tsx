455552858fd8833c9c2f1df9173d719e91047c22
palominodev <jeremypalomino.dev@gmail.com>
2025-03-26 15:00:18 -0500

    Arreglado de imagenes de productos

--- src/components/react/ListMenu.tsx
+++ src/components/react/ListMenu.tsx
@@ -1,42 +1,61 @@
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "../ui/tabs";
 import { PlatoCard } from "./PlatoCard";
+import CevicheIMG from "/public/ceviche_card.jpg";
+import TiraditoIMG from "/public/tiradito_card.jpg";
+import CausaIMG from "/public/causa_card.jpg";
+import AnticuchoIMG from "/public/anticucho.webp";
+
+import LomoSaltadoIMG from "/public/lomo_saltado.avif";
+import AjiGallinaIMG from "/public/aji_gallina_card.webp";
+import SecoFrejolesIMG from "/public/seco_frejoles_card.jpg";
+import ArrozPolloIMG from "/public/arrozconpollo_card.png";
+
+import SuspiroLimena from "/public/suspiro_limena_card.jpg";
+import MazamorraMorada from "/public/mazamorra_morada_card.webp";
+import ArrozLeche from "/public/arroz_con_leche.avif";
+import TurronPepa from "/public/turron_dona_pepa.jpg";
+
+import ChichaMorada from "/public/chicha_morada.jpg";
+import InkaKola from "/public/inca_kola.webp";
+import PiscoSour from "/public/pisco-sour_card.jpg";
+import Chilcano from "/public/chilcano_card.jpg";
 
 export const ListMenu = () => {
   const menu = [
     {
       category: "entradas",
       items: [
-        { name: "Ceviche", description: "Pescado fresco con limón", price: 15 },
-        { name: "Tiradito", description: "Pescado fresco con limón", price: 12 },
-        { name: "Causa", description: "Papa amarilla con ají", price: 10 },
-        { name: "Anticucho", description: "Corazón de res", price: 8 },
+        { name: "Ceviche", description: "Pescado fresco con limón", price: 15, img: CevicheIMG },
+        { name: "Tiradito", description: "Pescado fresco con limón", price: 12, img: TiraditoIMG },
+        { name: "Causa", description: "Papa amarilla con ají", price: 10, img: CausaIMG },
+        { name: "Anticucho", description: "Corazón de res", price: 8, img:AnticuchoIMG},
       ],
     },
     {
       category: "platos principales",
       items: [
-        { name: "Lomo saltado", description: "Carne de res con verduras", price: 20 },
-        { name: "Aji de gallina", description: "Gallina con ají amarillo", price: 18 },
-        { name: "Seco con frejoles", description: "Carne de res con frejoles", price: 15 },
-        { name: "Arroz con pollo", description: "Pollo con arroz", price: 12 },
+        { name: "Lomo saltado", description: "Carne de res con verduras", price: 20, img: LomoSaltadoIMG },
+        { name: "Aji de gallina", description: "Gallina con ají amarillo", price: 18, img: AjiGallinaIMG },
+        { name: "Seco con frejoles", description: "Carne de res con frejoles", price: 15, img: SecoFrejolesIMG },
+        { name: "Arroz con pollo", description: "Pollo con arroz", price: 12, img: ArrozPolloIMG },
       ],
     },
     {
       category: "postres",
       items: [
-        { name: "Suspiro a la limeña", description: "Dulce de leche con merengue", price: 8 },
-        { name: "Mazamorra morada", description: "Maíz morado con frutas", price: 6 },
-        { name: "Arroz con leche", description: "Arroz con leche", price: 5 },
-        { name: "Turrón de doña Pepa", description: "Dulce de harina con miel", price: 4 },
+        { name: "Suspiro a la limeña", description: "Dulce de leche con merengue", price: 8, img: SuspiroLimena },
+        { name: "Mazamorra morada", description: "Maíz morado con frutas", price: 6, img: MazamorraMorada },
+        { name: "Arroz con leche", description: "Arroz con leche", price: 5, img: ArrozLeche },
+        { name: "Turrón de doña Pepa", description: "Dulce de harina con miel", price: 4, img: TurronPepa },
       ],
     },
     {
       category: "bebidas",
       items: [
-        { name: "Chicha morada", description: "Maíz morado", price: 4 },
-        { name: "Inka Kola", description: "Gaseosa de sabor", price: 3 },
-        { name: "Pisco sour", description: "Pisco con limón", price: 6 },
-        { name: "Chilcano", description: "Pisco con ginger ale", price: 7 },
+        { name: "Chicha morada", description: "Maíz morado", price: 4, img: ChichaMorada },
+        { name: "Inka Kola", description: "Gaseosa de sabor", price: 3, img: InkaKola },
+        { name: "Pisco sour", description: "Pisco con limón", price: 6, img: PiscoSour },
+        { name: "Chilcano", description: "Pisco con ginger ale", price: 7, img: Chilcano },
       ],
     },
   ];
@@ -66,7 +85,7 @@ export const ListMenu = () => {
             <PlatoCard
               key={item.name}
               nombre={item.name}
-              img="https://via.placeholder.com/250"
+              img={item.img}
               description={item.description}
               precio={item.price}
             />
@@ -80,7 +99,7 @@ export const ListMenu = () => {
             <PlatoCard
               key={item.name}
               nombre={item.name}
-              img="https://via.placeholder.com/250"
+              img={item.img}
               description={item.description}
               precio={item.price}
             />
@@ -94,7 +113,7 @@ export const ListMenu = () => {
             <PlatoCard
               key={item.name}
               nombre={item.name}
-              img="https://via.placeholder.com/250"
+              img={item.img}
               description={item.description}
               precio={item.price}
             />
@@ -108,7 +127,7 @@ export const ListMenu = () => {
             <PlatoCard
               key={item.name}
               nombre={item.name}
-              img="https://via.placeholder.com/250"
+              img={item.img}
               description={item.description}
               precio={item.price}
             />
--- src/components/react/PlatoCard.tsx
+++ src/components/react/PlatoCard.tsx
@@ -2,13 +2,19 @@ type Props = {
   nombre: string;
   description: string;
   precio: number;
-  img: string;
+  img: ImageMetadata;
 };
 export const PlatoCard = ({ nombre, description, precio, img }: Props) => {
   return (
     <article className="border p-2 rounded-lg shadow-md">
       <figure>
-        <img src={img} alt={`plato de ${nombre}`} width={250} height={250} />
+        <img
+          src={img.src}
+          className="w-full h-48 object-cover rounded-lg"
+          alt={`plato de ${nombre}`}
+          width={250}
+          height={250}
+        />
         <figcaption>
           <h3 className="font-bold text-center">{nombre}</h3>
           {/* <p>{description}</p> */}
